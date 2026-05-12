20d48c6af87854a645fd0249b68ce488ca00c5bd
bekprokuror <kreauchun@gmail.com>
2026-05-12 10:27:30 +0500

    Update register.js

--- legistman/pages/register.js
+++ legistman/pages/register.js
@@ -20,7 +20,7 @@ export default function Register() {
   const loadTelegramWidget = () => {
     const script = document.createElement('script')
     script.src = 'https://telegram.org/js/telegram-widget.js?22'
-    script.setAttribute('data-telegram-login', 'LEGISTMAN_bot')
+    script.setAttribute('data-telegram-login', 'legistman_kursbot')
     script.setAttribute('data-size', 'large')
     script.setAttribute('data-radius', '8')
     script.setAttribute('data-onauth', 'onTelegramAuth(user)')
